3a1666489bfe7535db83eea72b3c2a8c964683bc
Sandhep <b.sandhep111@gmail.com>
2025-06-16 03:12:08 +0530

    updated route

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import AuthController from '../controllers/AuthController.js';
+import AuthMiddleware from '../middleware/AuthMiddleware.js';
 
 const router = express.Router();
 
 router.post('/login', AuthController.login);
 router.post('/register',AuthController.register);
+router.post('/invite-user',AuthMiddleware.authenticateToken,AuthController.inviteUser);
 
 export default router;
