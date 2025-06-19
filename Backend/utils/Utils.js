28131e1a7541f6a873e2913deabd7032d517df59
Sandhep <b.sandhep111@gmail.com>
2025-06-20 00:20:19 +0530

    email validator

--- Backend/utils/Utils.js
+++ Backend/utils/Utils.js
@@ -1,7 +1,13 @@
 
 class Utils {
 
-  
+   isValidEmail(email) {
+    if (!email || typeof email !== 'string') return false;
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
 }
 
 export default new Utils();
