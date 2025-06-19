1e94d7d07359f3c8084df5181cfe21c133ddec2f
Sandhep <b.sandhep111@gmail.com>
2025-06-20 00:09:58 +0530

    remove console.log

--- Backend/middleware/errorHandler.js
+++ Backend/middleware/errorHandler.js
@@ -2,8 +2,6 @@ export default function errorHandler(err, req, res, next) {
   const status = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
 
-  console.error(`[Error]: ${err.name} - ${message}`);
-
   res.status(status).json({
     success: false,
     error: message
