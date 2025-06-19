dfeba1e528579d8953902c5034e1fb772c73a334
Sandhep <b.sandhep111@gmail.com>
2025-06-20 00:35:34 +0530

    added rate limiter

---
+++ Backend/middleware/rateLimiter.js
@@ -0,0 +1,15 @@
+// /middlewares/rateLimiter.js
+import rateLimit from 'express-rate-limit';
+
+const apiRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100,                 // limit each IP to 100 requests per windowMs
+  message: {
+    success: false,
+    error: 'Too many requests, please try again later.',
+  },
+  standardHeaders: true,   // return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false,    // disable `X-RateLimit-*` headers
+});
+
+export default apiRateLimiter;
