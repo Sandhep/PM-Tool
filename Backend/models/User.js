2f5d6aac3208835e63e1bad7dafe8ea398840cf4
Sandhep <b.sandhep111@gmail.com>
2025-06-21 01:37:31 +0530

    schema update

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -8,6 +8,8 @@ const UserSchema = new mongoose.Schema({
   email:         { type: String, required: true, unique: true, lowercase: true, trim: true },
   passwordHash:  { type: String, required: true },
   role:         { type: [String], enum: ['user','admin'], default: ['user'] },
+  resetOtp: { type: String, default: null },
+  resetOtpExpiresAt: { type: Date, default: null },
   createdAt:    { type: Date, default: Date.now },
   updatedAt:    { type: Date, default: Date.now }
 });
