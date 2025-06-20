4faf096a52e37c0586e77445d406787c147d481f
Sandhep <b.sandhep111@gmail.com>
2025-06-21 01:38:09 +0530

    created user service

---
+++ Backend/services/UserService.js
@@ -0,0 +1,57 @@
+import crypto from 'crypto';
+import BadRequestException from '../exceptions/BadRequestException.js';
+import MailService from './MailService.js';
+import UserRepository from '../repositories/UserRepository.js';
+import InvitationRepository from '../repositories/InvitationRepository.js';
+import AuthService from './AuthService.js';
+
+class UserService{
+
+    async inviteUser(inviteUserDTO) {
+
+        const token = crypto.randomBytes(32).toString('hex');
+        const expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24); // 24 hrs
+    
+        await MailService.sendInviteMail(inviteUserDTO, token);
+    
+        const invitation = await InvitationRepository.create({
+          email: inviteUserDTO.email,
+          projectId: inviteUserDTO.projectId,
+          invitedBy: inviteUserDTO.invitedBy,
+          token,
+          role: inviteUserDTO.role,
+          expiresAt,
+        });
+    
+        return invitation;
+    }
+    
+    async acceptInvitation(acceptInviteDTO) {
+
+        const invite = await InvitationRepository.findByStatus(acceptInviteDTO.token, 'Pending');
+    
+        if (!invite || invite.expiresAt < new Date()) {
+          throw new BadRequestException("Invitation expired or invalid");
+        }
+    
+        let user = await UserRepository.findByEmail(invite.email);
+    
+        if (!user) {
+          user = await AuthService.register({
+            name: acceptInviteDTO.name,
+            email: invite.email,
+            password: acceptInviteDTO.password
+          });
+        }
+    
+        // TODO: Add user to project
+        // await ProjectService.addUserToProject(user._id, invite.projectId, invite.role);
+    
+        await InvitationRepository.updateStatus(acceptInviteDTO.token, 'Accepted');
+    
+        return user;
+    }
+
+}
+
+export default new UserService();
