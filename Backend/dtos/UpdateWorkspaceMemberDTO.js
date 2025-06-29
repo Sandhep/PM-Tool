2acfa809c07bf93de97f8d9067f10fd6a0ddd5de
Sandhep <b.sandhep111@gmail.com>
2025-06-30 01:23:16 +0530

    bug fix

--- Backend/dtos/UpdateWorkspaceMemberDTO.js
+++ Backend/dtos/UpdateWorkspaceMemberDTO.js
@@ -4,7 +4,7 @@ export default class UpdateWorkspaceMemberDTO {
 
   constructor(data) {
     
-    if (!data.userId || typeof data.userdId !== 'string') {
+    if (!data.userId || typeof data.userId !== 'string') {
       throw new BadRequestException('userId is required and must be string');
     }
 
