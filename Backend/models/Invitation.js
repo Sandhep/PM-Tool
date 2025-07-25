96ca8eb1a2271a3d2781a702face37a7d9e67b11
Sandhep <b.sandhep111@gmail.com>
2025-07-25 23:54:55 +0530

    changes related to workspace

--- Backend/models/Invitation.js
+++ Backend/models/Invitation.js
@@ -4,10 +4,9 @@ import { v4 as uuidv4 } from 'uuid';
 const invitationSchema = new mongoose.Schema({
   invitationId: { type: String, default: uuidv4, unique: true, index: true },
   email: { type: String, required: true },
-  projectId: { type: String, required: true },
+  workspaceId: { type: String,ref:'Workspace', required: true },
   invitedBy: { type: String, ref: 'User', required: true },
-  scope: { type: String, enum: ['Full', 'Restricted', 'ReadOnly'], default: 'Full', },
-  role: { type: String, default: 'Developer' },
+  role: { type: String, enum:['Admin','Member'],default: 'Member' },
   token: { type: String, required: true },
   status: { type: String, enum: ['Pending', 'Accepted', 'Expired'], default: 'Pending' },
   expiresAt: { type: Date, required: true },
