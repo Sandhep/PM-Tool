3e75c7e7d8bc28c18805429d2741d62486b6ed7e
Sandhep <b.sandhep111@gmail.com>
2025-07-30 01:13:58 +0530

    implemented task rbac

--- Backend/routes/taskRoutes.js
+++ Backend/routes/taskRoutes.js
@@ -2,7 +2,8 @@ import express from 'express';
 import AuthMiddleware from '../middleware/AuthMiddleware.js';
 import TaskController from '../controllers/TaskController.js';
 import contentTypeMiddleware from '../middleware/ContentTypeMiddleware.js';
-//import AccessControlMiddleware from '../middleware/AccessControlMiddleware.js';
+import TaskAccessControlMiddleware from '../middleware/TaskAccessControlMiddleware.js';
+import ProjectAccessControlMiddleware from '../middleware/ProjectAccessControlMiddleware.js';
 
 const router = express.Router();
 
@@ -11,38 +12,35 @@ router.use(contentTypeMiddleware.allow(['application/json']));
 router.post(
   '/',
   AuthMiddleware.authenticateToken,
-  //AccessControlMiddleware.checkAccess,
-  //AccessControlMiddleware.checkScope('edit'),
+  ProjectAccessControlMiddleware.checkRole(['Admin','Manager','Collaborator']),
   TaskController.create
 );
 
 router.get(
   '/',
-  //AuthMiddleware.authenticateToken,
-  //AccessControlMiddleware.checkAccess,
   AuthMiddleware.authenticateToken,
+  ProjectAccessControlMiddleware.checkRole(['Admin','Manager','Collaborator','Viewer']),
   TaskController.list
 );
 
 router.get(
   '/:taskId',
   AuthMiddleware.authenticateToken,
+  TaskAccessControlMiddleware.checkRole(['Admin','Manager','Collaborator','Viewer']),
   TaskController.fetch
 );
 
 router.put(
   '/:taskId',
   AuthMiddleware.authenticateToken,
-  //AccessControlMiddleware.checkAccess,
-  //AccessControlMiddleware.checkScope('edit'),
+  TaskAccessControlMiddleware.checkRole(['Admin','Manager','Assigner','Assignee']),
   TaskController.update
 );
 
 router.delete(
   '/:taskId',
   AuthMiddleware.authenticateToken,
-  //AccessControlMiddleware.checkAccess,
-  //AccessControlMiddleware.checkScope('full'),
+  TaskAccessControlMiddleware.checkRole(['Admin','Manager','Assigner']),
   TaskController.delete
 );
 
