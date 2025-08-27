8febb9499830a65c8f61f68b04be6be34d820c37
Sandhep <b.sandhep111@gmail.com>
2025-08-28 00:19:54 +0530

    added activity log

--- Backend/modules/Task/controller/TaskController.js
+++ Backend/modules/Task/controller/TaskController.js
@@ -1,4 +1,6 @@
 import TaskService from '../service/TaskService.js';
+import ActivityLog from '../../ActivityLog/service/ActivityLogService.js'
+import ActivityLogConstants from '../../../common/constants/ActivityLogConstants.js';
 import CreateTaskDTO from '../dto/CreateTaskDTO.js';
 import UpdateTaskDTO from '../dto/UpdateTaskDTO.js';
 import FilterTaskDTO from '../dto/FilterTaskDTO.js';
@@ -17,6 +19,13 @@ class TaskController {
     try {
       const dto = new CreateTaskDTO({ ...req.body, assignerId: req.user.userId , projectId:req.query.projectId});
       const task = await TaskService.createTask(dto);
+      ActivityLog.recordActivity({
+         resourceType: ActivityLogConstants.RESOURCE_TYPE.TASK,
+         resourceId: task.taskId,
+         actionType: ActivityLogConstants.ACTION_TYPE.CREATED,
+         description: "Task created",
+         initiatedBy: req.user.userId,
+      })
       res.status(201).json({ message: "Task created successfully", task });
     } catch (error) {
       next(error);
@@ -27,7 +36,31 @@ class TaskController {
     try {
       const dto = new UpdateTaskDTO(req.body);
       const task = await TaskService.updateTask(req.params.taskId, dto);
+      ActivityLog.recordActivity({
+         resourceType: ActivityLogConstants.RESOURCE_TYPE.TASK,
+         resourceId: req.params.taskId,
+         actionType: ActivityLogConstants.ACTION_TYPE.UPDATED,
+         description: "Task Updated",
+         initiatedBy: req.user.userId,
+      })
       res.status(200).json({ message: "Task updated successfully", task });
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async updateTaskAssignee(req, res, next) {
+     try {
+      const assigneeId = req.body.assigneeId;
+      const task = await TaskService.updateTaskAssignee(req.params.taskId, assigneeId);
+      ActivityLog.recordActivity({
+         resourceType: ActivityLogConstants.RESOURCE_TYPE.TASK,
+         resourceId: req.params.taskId,
+         actionType: ActivityLogConstants.ACTION_TYPE.UPDATED,
+         description: "Task Assignee Updated",
+         initiatedBy: req.user.userId,
+      })
+      res.status(200).json({ message: "Task Assignee updated successfully", task });
     } catch (error) {
       next(error);
     }
@@ -36,6 +69,13 @@ class TaskController {
   async delete(req, res, next) {
     try {
       await TaskService.deleteTask(req.params.taskId);
+      ActivityLog.recordActivity({
+         resourceType: ActivityLogConstants.RESOURCE_TYPE.TASK,
+         resourceId: req.params.taskId,
+         actionType: ActivityLogConstants.ACTION_TYPE.DELETED,
+         description: "Task Deleted",
+         initiatedBy: req.user.userId,
+      })
       res.status(200).json({ message: "Task deleted successfully" });
     } catch (error) {
       next(error);
